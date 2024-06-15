d25cc6a07e1d6f8da7df553b870e662863188d1d
Danusha Pramode <Danusha Pramode@LAPTOP-BUHIP5FG>
2024-06-15 13:49:17 +0530

    First Commit

---
+++ src/Project01/car.java
@@ -0,0 +1,30 @@
+package Project01;
+
+public class car {
+
+	int no;                   //instance variables
+	String name;   
+	String color;
+	double weight;
+	int speed;
+	
+	
+	public void speed() {              //instance methods
+		System.out.println(speed);
+	}
+	
+	public void color() {
+		System.out.println(color);
+	}
+
+	public void weight() {
+		System.out.println(weight);
+		
+		
+	}
+	
+	public void name() {
+		System.out.println(name);
+		
+	}
+}
---
+++ src/Project01/main.java
@@ -0,0 +1,22 @@
+package Project01;
+
+public class main {
+
+	public static void main(String[] args) {
+		
+		car c1 = new car();  //create object
+		
+		c1.color = "Red";    //initialize attributes
+		c1.name = "Toyota";
+		c1.speed = 150;
+		c1.weight = 45.3;
+		c1.no = 123;
+		
+		c1.speed();  //Call methods
+		c1.color();
+		c1.weight();
+		c1.name();
+
+	}
+
+}
